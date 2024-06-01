d874f2b5dea7314f16ffa8780d5a91d8065a2a76
Marvin <aguirreestuardo758@gmail.com>
2024-06-01 10:51:20 -0600

    first commit

---
+++ Frontend/inventory_management_system/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container-fluid p-5'>
+        <h1>Administrador de Productos - MERN CRUD by duende</h1>
+    </div>
+  )
+}
